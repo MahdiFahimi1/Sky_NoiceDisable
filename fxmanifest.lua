1a3871e53dab072e14b333b5d9ebf78cf4e01582
MahdiFahimi1 <m.mahdi.lind@gmail.com>
2024-07-16 04:51:03 -0700

    File Uploaded
    Script File Uploaded

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+description 'Script By | Discord ID : mahdifahimi'
+version '1.0.0'
+author '!-MF-! | Discord ID : mahdifahimi'
+
+
+client_script 'client.lua'
